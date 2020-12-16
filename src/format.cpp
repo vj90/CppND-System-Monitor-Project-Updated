2c46158eb942f5829c3d62e78f5c2f87e8d4c424
Vasundhara Jain <vasundhara.jain@daimler.com>
2020-12-16 18:57:12 +0100

    Implement time format helper function

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,21 @@
 
 using std::string;
 
-// TODO: Complete this helper function
+// Helper function to format time
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  constexpr int seconds_in_minute = 60;
+  constexpr int seconds_in_hour = 3600;
+  const int hours = seconds / seconds_in_hour;
+  const string hrs =
+      hours < 10 ? "0" + std::to_string(hours) : std::to_string(hours);
+  seconds = seconds % seconds_in_hour;
+  const int minutes = seconds / seconds_in_minute;
+  const string min =
+      minutes < 10 ? "0" + std::to_string(minutes) : std::to_string(minutes);
+  seconds = seconds % seconds_in_minute;
+  const string sec =
+      seconds < 10 ? "0" + std::to_string(seconds) : std::to_string(seconds);
+  return hrs + ":" + min + ":" + sec;
+}
